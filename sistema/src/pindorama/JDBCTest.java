be417229969e962c3107d4de8c47f3bfe3a11391
Marco Mangan <marco.mangan@gmail.com>
2013-08-22 09:01:10 -0300

    Configuração de conexão
    Inclusão de arquivo de properties.

--- sistema/src/pindorama/JDBCTest.java
+++ sistema/src/pindorama/JDBCTest.java
@@ -1,15 +1,20 @@
 package pindorama;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
 
 public class JDBCTest {
 
-	public static void main(String[] args) throws SQLException {
-		String url = "jdbc:postgresql://localhost:5432/pindorama";
-		String username = "postgres";
-		String password = "senacrs";
-		
-		Connection db = DriverManager.getConnection(url, username, password);
+	// http://jdbc.postgresql.org/documentation/head/connect.html
+	public static void main(String[] args) throws SQLException, FileNotFoundException, IOException {
+		Properties props = new Properties();
+		props.load(new FileInputStream("pindorama.properties"));
+		String url = props.getProperty("url");
+			
+		Connection db = DriverManager.getConnection(url, props);
 		System.out.println("Conectado!");
 	}
 }
